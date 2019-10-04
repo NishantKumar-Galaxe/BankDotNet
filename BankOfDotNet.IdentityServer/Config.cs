eca8f0a111ff7b9e6b8e70cc604daa8b8b8d6f81
Nishant Kumar <nkumar1@galaxe.com>
2019-10-04 18:30:13 +0530

    Add project files.

---
+++ BankOfDotNet.IdentityServer/Config.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfDotNet.IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("BankOfDotNetAPI","Customer API for BankOfDotNet")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId= "client",
+                    AllowedGrantTypes= GrantTypes.ClientCredentials,
+                    ClientSecrets=
+                    {
+                        new Secret("password".Sha256())
+                    },
+                    AllowedScopes={ "BankOfDotNetAPI" }
+                }
+            };
+        }
+    }
+}
