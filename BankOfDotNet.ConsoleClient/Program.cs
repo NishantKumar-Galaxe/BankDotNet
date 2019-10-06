aea99989e07d8acef6ccba3b7605635b0cf405cc
Nishant Kumar <nkumar1@galaxe.com>
2019-10-06 14:55:37 +0530

    Commit Client

---
+++ BankOfDotNet.ConsoleClient/Program.cs
@@ -0,0 +1,72 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOfDotNet.ConsoleClient
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            //Discover all endpoints using metadata of identity server
+
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            //Grab a bearer Token
+            var TokenClient = new TokenClient(disco.TokenEndpoint, "client", "password");
+            var tokenResponse = await TokenClient.RequestClientCredentialsAsync("BankOfDotNetAPI");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            //Consume our Customer API
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var customerInfo = new StringContent(
+
+                JsonConvert.SerializeObject(
+                    new { Id = 10, FirstName = "Nishant", LastName = "Kumar" }),
+                Encoding.UTF8, "application/json");
+
+            var createCustomerResponse = await client.PostAsync("http://localhost:56000/api/customers"
+                , customerInfo);
+
+            if (!createCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(createCustomerResponse.StatusCode);
+            }
+
+            var getCustomerResponse = await client.GetAsync("http://localhost:56000/api/customers");
+            if (!getCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(getCustomerResponse.StatusCode);
+            }
+            else
+            {
+                var content = await getCustomerResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.Read();
+        }
+    }
+}
